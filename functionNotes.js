78b9cc5c8f4f33db09f839007f0c019e281ed933
nwb8 <nwb8@fresnou.org>
2023-03-14 10:30:18 -0700

    initial commit

--- functionNotes.js
+++ functionNotes.js
@@ -1,34 +1,34 @@
-// //functions notes
-// //step 1: Define the function
-// function sayHello(){
-//     console.log("Hello");
-// }
-// //Step 2: Call (or us) the function
-// sayHello();
+//functions notes
+//step 1: Define the function
+function sayHello(){
+    console.log("Hello");
+}
+//Step 2: Call (or us) the function
+sayHello();
 
-// function printMax(){
-//     let x = 6;
-//     let y = 29;
-//     if (x > y){
-//         console.log(`The larger the number is ${x}.`)
-//     } else {
-//         console.log(`The Larger the number is ${y}.`)
-//     }
-// }
-//
-// printMax();
+function printMax(){
+    let x = 6;
+    let y = 29;
+    if (x > y){
+        console.log(`The larger the number is ${x}.`)
+    } else {
+        console.log(`The Larger the number is ${y}.`)
+    }
+}
 
-// function printMax(x, y){
-//     if (x > y){
-//         console.log(`The larger the number is ${x}.`)
-//     } else {
-//         console.log(`The Larger the number is ${y}.`)
-//     }
-// }
-//
-// printMax( 16, 9);
-// printMax( 2, 75);
-// printMax( 2, 2);
+printMax();
+
+function printMax(x, y){
+    if (x > y){
+        console.log(`The larger the number is ${x}.`)
+    } else {
+        console.log(`The Larger the number is ${y}.`)
+    }
+}
+
+printMax( 16, 9);
+printMax( 2, 75);
+printMax( 2, 2);
 
 function sum(num1, num2){
     console.log(`${num1}+ ${num2} = ${num1 + num2}`);
---
+++ functionPractice.js
@@ -0,0 +1,33 @@
+//Question 1
+// function findAreaOfRectangle(num1, num2){
+//         console.log(`${num1} * ${num2} = ${num1 * num2}`)
+//     }
+// findAreaOfRectangle(5, 3);
+
+//Question2
+// function findAreaOfCircle(num1, pi){
+//     console.log(`${num1} * ${pi} = ${num1 * pi}`)
+// }
+// findAreaOfCircle(60,3.14)
+
+//Question3
+// function printMax(e, a){
+//     if (e > a){
+//         console.log(`The larger the number is ${e}.`)
+//     } else {
+//         console.log(`The Larger the number is ${a}.`)
+//     }
+// }
+//
+// printMax( 15, 13);
+
+//Question4
+function printMax(e, a){
+    if (e > a){
+        console.log(`Their both equal ${a} and ${e}.`)
+    } else {
+        console.log(`The Larger the number is ${e} and not ${a} .`)
+    }
+}
+
+printMax( 15, 15);
