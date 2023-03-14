2c7bff78ad420c80c39b15abbf9788d06e6ce45b
nwb8 <nwb8@fresnou.org>
2023-03-14 10:00:56 -0700

    initial commit

---
+++ functionNotes.js
@@ -0,0 +1,38 @@
+// //functions notes
+// //step 1: Define the function
+// function sayHello(){
+//     console.log("Hello");
+// }
+// //Step 2: Call (or us) the function
+// sayHello();
+
+// function printMax(){
+//     let x = 6;
+//     let y = 29;
+//     if (x > y){
+//         console.log(`The larger the number is ${x}.`)
+//     } else {
+//         console.log(`The Larger the number is ${y}.`)
+//     }
+// }
+//
+// printMax();
+
+// function printMax(x, y){
+//     if (x > y){
+//         console.log(`The larger the number is ${x}.`)
+//     } else {
+//         console.log(`The Larger the number is ${y}.`)
+//     }
+// }
+//
+// printMax( 16, 9);
+// printMax( 2, 75);
+// printMax( 2, 2);
+
+function sum(num1, num2){
+    console.log(`${num1}+ ${num2} = ${num1 + num2}`);
+}
+
+sum(7, 10);
+sum(27, 1);
