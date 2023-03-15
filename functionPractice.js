4d17f38c0d27507cc3b92b5470ea90deadb1a2be
nwb8 <nwb8@fresnou.org>
2023-03-15 10:32:28 -0700

    initial commit

--- functionPractice.js
+++ functionPractice.js
@@ -4,7 +4,7 @@
 //     }
 // findAreaOfRectangle(5, 3);
 
-//Question2
+//Question 2
 // function findAreaOfCircle(num1, pi){
 //     console.log(`${num1} * ${pi} = ${num1 * pi}`)
 // }
@@ -21,13 +21,55 @@
 //
 // printMax( 15, 13);
 
-//Question4
-function printMax(e, a){
-    if (e > a){
-        console.log(`Their both equal ${a} and ${e}.`)
-    } else {
-        console.log(`The Larger the number is ${e} and not ${a} .`)
-    }
-}
+//Question 4
+// function printMax(e, a){
+//     if (e = a){
+//         console.log(`Their both equal ${a} and ${e}.`)
+//     } if (e > a) {
+//         console.log(`The Larger the number is ${e} and not ${a} there both not equal.`)
+//     } if (e < a){
+//         console.log(`The larger number is ${a} and not ${e} there both not equal`)
+//     }
+// }
+//
+// printMax( 32, 1);
 
-printMax( 15, 15);
+//Question 5
+// function sum(num1, num2,){
+//     console.log(`${num1}*${num2}=${num1*num2}`);}
+//     sum1(23, 31);
+//     function sum1(num3, num4){
+//         console.log(`${num3}*${num4}=${num3*num4}`);}
+//         sum1(54, 28);function printMax() {
+//     let sum = 713;
+//     let sum1 = 1512;
+//     if (sum > sum1) {
+//         console.log(`the largest area is ${sum}`);
+//     } else {
+//         console.log(`the largest area is ${sum1}`);
+//     }
+// }
+// printMax();
+
+//Question 6
+// function sum(num, num2, num3){
+//     console.log(`${num} + ${num2} + ${num3} = ${num + num2 + num3}`);
+// } sum(450, 300, 500);
+// let sum1 = 1250;
+// if (sum1 > 1000){
+//     console.log(sum1 * 2);
+//     console.log(`The charity will receive $2500`)
+// }
+
+//Question 7
+// let water = 50;
+// if (water > 110) {
+//     console.log(`The water is boiling!`)
+// }
+// else if (water < 40) {
+//     console.log(`The water is slowly freezing!`)
+// }
+// else if (water > 200) {
+//     console.log(`The water is going to evaporate soon!`)
+// }
+
