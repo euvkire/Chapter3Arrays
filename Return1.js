f5a0b21938c81b1f71c5b001d8bbef02c7956bdf
nwb8 <nwb8@fresnou.org>
2023-03-17 10:29:20 -0700

    initial commit

--- Return1.js
+++ Return1.js
@@ -7,3 +7,29 @@ function nameSwapper(){
     return(`${last}, ${first}`)
 }
 console.log(nameSwapper());
+
+//2
+function numGen(){
+    let num1 = readline.question( "Number One: ");
+    let num2 = readline.question("Number Two: ");
+    return(`${num1}${num2}`)
+}
+console.log(numGen());
+
+//3
+function tempa(d1, d2, d3, d4, d5,  d6, d7){
+    console.log(`${d1} + ${d2} + ${d3} + ${d4} + ${d5} + ${d6} + ${d7} / 14 = ${d1 + d2 + d3 + d4 + d5 + d6 + d7 + 45 + 32 + 36 + 46 + 44 + 43 + 44 * 0.001}`);
+}
+tempa(80, 78, 54, 67, 65, 51, 43);
+
+//4
+function sum(num, num2, num3){
+    console.log(`${num} + ${num2} + ${num3} = ${num + num2 + num3}`);
+} sum(450, 300, 500);
+let sum1 = 1250;
+if (sum1 > 1000){
+    console.log(sum1 * 2);
+    console.log(`The charity will receive $2500`)}
+else if(sum1 > 2000){
+    console.log(sum1 * 2);
+}
