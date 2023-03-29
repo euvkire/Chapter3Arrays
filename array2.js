ab64215e1f9eebf1197ed6dad76235d63d20be5d
nwb8 <nwb8@fresnou.org>
2023-03-29 10:18:22 -0700

    initial commit

--- array2.js
+++ array2.js
@@ -5,25 +5,19 @@ console.log('if n =5: ' + numb);
 console.log('if n =1: ' + newNumb);
 
 //2
-let nums = [1, 2, 3, 4, 5];
-let newNums = [1, 2];
-let n = 0;
-console.log('if n = 5: ' + nums);
-console.log('if n = 1: ' + newNums);
-console.log('if n = 0: invalid');
+function Count(n){
+    if (n <= 0){
+        console.log('Invalid')
+    }
+    else( n >=0);
+    let numbaList = [];
+    for (let i=1; i<=n; i++){
+        numbaList.push(i);
+    }
+    console.log(numbaList)
+}
+Count(-1);
 
 //3
-let highScores = [107, 99, 98];
-let newScore = 101;
-let newHighScores = [];
-for (let x=0; x<highScores.length; x++){
-    if (highScores[x]>= newScore){
-        newHighScores.push(highScores[x])
-    } else{
-        newHighScores.push(newScore);
-        newScore=0
-    }
-}
-console.log(newHighScores);
 
-//4
+
