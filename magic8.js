2106bed2f5855ce854bba90cd99ec4e3e87ef01d
nwb8 <nwb8@fresnou.org>
2023-04-13 10:20:53 -0700

    initial commit

---
+++ magic8.js
@@ -0,0 +1,12 @@
+let answerList = ['yes', 'no', 'maybe'];
+
+let randomNum = Math.floor(Math.random()*answerList.length);
+console.log(randomNum);
+let randomAnswer = answerList[randomNum];
+console.log(randomAnswer);
+
+//magic8 ball :D
+
+let answerlist = ['That\'s cool', 'How wonderful!', 'Dang that sucks'];
+
+let randomQues  = 
