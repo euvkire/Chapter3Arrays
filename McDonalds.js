f85ba2f1608655f9c27d71708cca9c143baec84e
nwb8 <nwb8@fresnou.org>
2023-04-27 09:15:10 -0700

    initial commit

--- McDonalds.js
+++ McDonalds.js
@@ -8,14 +8,20 @@ while (answer != "chicken" && answer != "beef" && answer != "tofu"){
         console.log("invalid choice");
     }
 }
-let answer2 = readLine.question("Would you like a beverage with it?: ");
+let answer2 = readLine.question("Would you like a beverage with it?\nsmall $1.00, medium $1.75, large $2.25: ");
 while (answer2 != "yes" && answer2 != "no"){
     console.log("invalid choice");
-    answer2 = readLine.question("Would you like a beverage with it?: ");
+    answer2 = readLine.question("Repeat it?");
+    while (answer2 != "no" && answer2 != "yes");
+}  if (answer2 = "no"){
+console.log("No drink with your food.");}
+if (answer2 = "yes"){
+    console.log("Drinks with your food.")
 }
 
 let answer3 = readLine.question("Would you like a side of fries with it?: ");
 while (answer3 != "yes" && answer3 != "no"){
-    console.log("invalid choice");
+    console.log("invalid choice");}
+    if(answer3 = "yes"){
     answer3 = readLine.question("What size?(Small: $3.25, Medium: $5, Large: $6.50): ");
 }
