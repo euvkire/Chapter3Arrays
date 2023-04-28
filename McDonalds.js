e4f1f909c5e4e985eccbaac4fb5fb031f639e254
nwb8 <nwb8@fresnou.org>
2023-04-28 10:08:58 -0700

    initial commit

--- McDonalds.js
+++ McDonalds.js
@@ -8,15 +8,15 @@ while (answer != "chicken " && answer != "beef" && answer != "tofu"){
         console.log("invalid choice");
     }
 }
-let answer2 = readLine.question("Would you like a beverage with it or no?\nsmall $1.00, medium $1.75, large $2.25: ");
-while (answer2 != "yes" && answer2 != "no"  && answer2 != "small"  && answer2 != "medium"  && answer2 != "large"){
-    console.log("invalid choice");
-    answer2 = readLine.question("Repeat it?");
-    while (answer2 != "no" && answer2 != "yes" && answer2 != "small" && answer2 != "medium" && answer2 != "large");
-}  if (answer2 == "no"){
-    console.log("No drink with your food.");}
-if (answer2 == "yes" && answer2 == "small" && answer2 == "medium" && answer2 == "large"){
-    console.log("Drinks with your food.")
+let answer2 = readLine.question("Would you like a beverage?: ");
+while (answer2 != "yes" && answer2 != "no"){
+    console.log("invalid choice");}
+if (answer2 == "no"){
+    console.log("No Drinks")
+}
+if(answer2 == "yes"){
+    answer2 = readLine.question("What size?(Small: $4, Medium: $6, Large: $8): ");
+    console.log(`${answer2} Drink.`)
 }
 
 let answer3 = readLine.question("Would you like a side of fries with it?: ");
@@ -34,8 +34,8 @@ let number4 = Number(readLine.question("How many packet of ketch up do you want 
 if (number4 == "no"){
     console.log(`${number4} no ketchup packets for you. (Cost 0)`)
 }
-if (number4 == "yes"){
+
     let egg = number4 * 0.25;
     console.log(`${number4} ketchup packets for you. (Cost ${egg})`);
-    console.log(`${answer} burger, ${answer2} drink, ${answer3} Fries, ${number4} Packets of ketchup. (Cost ${egg})`);}
+    console.log(`${answer} burger, ${answer2} drink, ${answer3} Fries, ${number4} Packets of ketchup. (Cost ${egg})`);
 
