58ce4f5a097a69229643f16ca71aa2a6cc02553c
nwb8 <nwb8@fresnou.org>
2023-04-28 10:31:22 -0700

    initial commit

--- pt2cm.js
+++ pt2cm.js
@@ -1,12 +1,10 @@
 const readLine = require(`readline-sync`);
-
+let costs = []
 let answer = readLine.question("What type of burger would you like chicken $5.25, beef $6.25, or tofu $5.75?: ");
 while (answer != "chicken " && answer != "beef" && answer != "tofu"){
     console.log("invalid choice");
     answer = readLine.question("Repeat it? ");
-    while (answer != "sure" && answer != "yes"){
-        console.log("invalid choice");
-    }
+
 }
 if (answer == 'chicken'){
     costs.push(5.25)
@@ -17,15 +15,21 @@ if (answer == 'chicken'){
 } else {
     costs.push(0)
 }
-let answer2 = readLine.question("Would you like a beverage?: ");
-while (answer2 != "yes" && answer2 != "no"){
-    console.log("invalid choice");}
-if (answer2 == "no"){
-    console.log("No Drinks")
-}
-if(answer2 == "yes"){
-    answer2 = readLine.question("What size?(Small: $4, Medium: $6, Large: $8): ");
-    console.log(`${answer2} Drink.`)
+
+
+
+
+
+
+let answer2 = readLine.question("Would you like a beverage with it or no?\nsmall $1.00, medium $1.75, large $2.25: ");
+while (answer2 != "yes" && answer2 != "no" && answer2 != "small" && answer2 != "medium" && answer2 != "large"){
+    console.log("invalid choice");
+    answer2 = readLine.question("Repeat it?");
+    while (answer2 != "no" && answer2 != "yes" && answer2 != "small" && answer2 != "medium" && answer2 != "large");
+} if (answer2 == "no"){
+    console.log("No drink with your food.");}
+if (answer2 == "yes" && answer2 == "small" && answer2 == "medium" && answer2 == "large"){
+    console.log("Drinks with your food.")
 }
 if (answer == 'small'){
     costs.push(1.00)
@@ -36,6 +40,8 @@ if (answer == 'small'){
 } else {
     costs.push(0)
 }
+
+
 
 let answer3 = readLine.question("Would you like a side of fries with it?: ");
 while (answer3 != "yes" && answer3 != "no"){
@@ -56,13 +62,11 @@ if (answer == 'small'){
 } else {
     costs.push(0)
 }
-
 let number4 = Number(readLine.question("How many packet of ketch up do you want or you dont want any?: "));
 if (number4 == "no"){
     console.log(`${number4} no ketchup packets for you. (Cost 0)`)
 }
-
-    let egg = number4 * 0.25;
-    console.log(`${number4} ketchup packets for you. (Cost ${egg})`);
-    console.log(`${answer} burger/sandwich, ${answer2} drink, ${answer3} Fries, ${number4} Packets of ketchup. (Cost ${egg})`);
-
+if (number4 == "yes"){
+    let epic = number4 * 0.25;
+    console.log(`${number4} ketchup packets for you. (Cost ${epic})`);
+    console.log(`${answer} burger, ${answer2} drink, ${answer3} Fries, ${number4} Packets of ketchup. (Cost ${epic})`);}
